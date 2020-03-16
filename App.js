b80e367cf72cfdc04b649bbd44d8c2976641f21f
Olimpio Kaio <olimpiocode@gmail.com>
2020-03-16 14:55:26 -0300

    Adicionado o restart na tela de GameOverScreen

--- App.js
+++ App.js
@@ -10,13 +10,17 @@ export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
 
+  const configureNewGameHandler = () =>  {
+    setGuessRounds(0)
+    setUserNumber(null)
+  }
+
   const startGameHandler = (selectedNumber) => {
-    setUserNumber(selectedNumber);
-    setGuessRounds(0);
+    setUserNumber(selectedNumber)
   }
 
   const gameOverHandler = numOfRounds => {
-    setGuessRounds(numOfRounds);
+    setGuessRounds(numOfRounds)
   }
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
@@ -24,7 +28,7 @@ export default function App() {
   if (userNumber && guessRounds <= 0) {
     content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
   } else if (guessRounds > 0) {
-    content = <GameOverScreen />
+    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler} />
   }
 
   return (
